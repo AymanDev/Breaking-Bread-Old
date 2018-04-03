571daf7728218d3e3b6f9a68a2be62529a4eecd7
AymanDev <aimandeveloper@gmail.com>
2018-04-03 13:00:49 +0300

    v0.0.1b
    Добавлено меню паузы.

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    private GameObject pausePanelObject;
+
+    private void Start()
+    {
+        pausePanelObject = transform.FindChild("PausePanel").gameObject;
+    }
+
+    private void Update()
+    {
+        var selectCharacterMenu = transform.FindChild("Panel");
+        if (!selectCharacterMenu.gameObject.activeSelf && Input.GetButton("Cancel"))
+        {
+            Pause();
+        }
+    }
+
+    private void Pause()
+    {
+        pausePanelObject.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Unpause()
+    {
+        pausePanelObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
